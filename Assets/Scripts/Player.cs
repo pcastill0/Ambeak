18f01652b16b97fc5870508de0d0587cf3d71037
pcastill0 <pedro.castillo@enti.com>
2024-01-23 10:37:15 +0100

    Cambios mapa y movimiento

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,23 @@ public class Player : MonoBehaviour
     public float speed;
     public string inputHorizontal;
     public string inputVertical;
+    private Rigidbody2D rb;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis(inputHorizontal);
         float vertical = Input.GetAxis(inputVertical);
-        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
+        transform.position += new Vector3(horizontal, vertical, 0) * Time.fixedDeltaTime * speed;
+
+        //Vector2 movement = new Vector2(horizontal, vertical);
+       // movement.Normalize(); // Ensure diagonal movement is not faster
+
+       // rb.velocity = movement * speed;
     }
+
 }
