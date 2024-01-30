a6b5802100a47bd2561f10c8d41c5b173e6b9429
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2024-01-30 10:00:26 +0100

    animacion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,30 @@ public class Player : MonoBehaviour
     public float speed;
     public string inputHorizontal;
     public string inputVertical;
+    public Animator animator;
 
     void Start()
     {
-
+         
     }
     void Update()
     {
         float horizontal = Input.GetAxis(inputHorizontal);
         float vertical = Input.GetAxis(inputVertical);
-        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
+
+      transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * speed;
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            animator.SetFloat("X", horizontal);
+            animator.SetFloat("Y", vertical);
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+      
     }
 }
