c971e11cc79ed7ed65639aad12ea132897621c2e
ToniBorrull <146344011+ToniBorrull@users.noreply.github.com>
2024-03-04 14:24:28 +0100

    faltan hearts de la ia

--- Assets/seguirObjetivo.cs
+++ Assets/seguirObjetivo.cs
@@ -3,23 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class seguirObjetivo : MonoBehaviour
+public class IAEnemigo : MonoBehaviour
 {
 
     [SerializeField] private Transform objetivo;
+    [SerializeField] private Transform objetivo2;
 
     private NavMeshAgent navMeshAgent;
 
+    float counter;
+    int result;
+    int type;
 
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.updateRotation = false;
         navMeshAgent.updateUpAxis = false;
+        counter = 0;
+        result = Random.Range(0, 2); ;
+        
     }
 
     private void Update()
     {
-        navMeshAgent.SetDestination(objetivo.position);
+        counter -= Time.deltaTime;
+        if(counter <= 0)
+        {
+            result = Random.Range(0, 2);
+            if (result == 0)
+            {
+                type = 1;
+            }
+            else if (result == 1)
+            {
+                type = 2;
+            }
+            counter = 5;
+        }
+
+
+        
+        if (type == 1)
+        {
+            navMeshAgent.SetDestination(objetivo.position);
+        }
+        else if (type == 2)
+        {
+            navMeshAgent.SetDestination(objetivo2.position);
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+
+
+            collision.GetComponent<IAHearts>().health -= 1;
+
+            Destroy(this.gameObject);
+
+        }
     }
 }
