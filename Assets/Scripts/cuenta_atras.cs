8203d833634919298c85efd9b6e7192a6b5022d7
akkarugi <146344051+akkarugi@users.noreply.github.com>
2024-03-10 14:22:13 +0100

    CUENTA  ATRAS

---
+++ Assets/Scripts/cuenta_atras.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cuenta_atras : MonoBehaviour
+{
+    public GameObject uno;
+    public GameObject dos;
+    public GameObject tres;
+    public GameObject start;
+
+    void Start()
+    {
+        uno.SetActive(false);
+        dos.SetActive(false);
+        tres.SetActive(false);
+        start.SetActive(false);
+
+        StartCoroutine(StartCountdown());
+    }
+
+    public IEnumerator StartCountdown()
+    {
+        
+
+        yield return new WaitForSecondsRealtime(1f);
+        tres.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        tres.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1f);
+        dos.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        dos.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1f);
+        uno.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        uno.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(1f);
+        start.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        start.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+}
